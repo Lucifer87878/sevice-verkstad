2626a79b94465ee365aefce19c33fb91af5fa8c4
Lucifer <nallenrockar123@gmail.com>
2024-04-24 13:11:15 +0200

    förändring med sökvägar till olika sidor med route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import "../src/components/Scss/main.scss";
 import Navbar from "./components/Navbar/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Planering from "./pages/Planering/Planering";
+import ServiceProtokol from "./pages/ServiceProtokol/ServiceProtokol";
+import DifferentServices from "./pages/DifferentServices/DifferentServices";
+import CooperateWithOss from "./pages/CooperateWithOss/CooperateWithOss";
 import Card from "./components/Cards/Card";
+import DragAndDrop from "./components/DragDrop/DragAndDrop";
+import Footer from "./components/Footer/Footer";
 
-
-//-------------------------------------------NavBar-----------------------------------------------//
 const GoToNavLink = [
-  { Text: "Home", href: "/#" },
-  { Text: "Planering", href: "/#" },
-  { Text: "Service Protokol", href: "/#" },
-  { Text: "Different services", href: "/#" },
-  { Text: "Cooperate with oss", href: "/#" },
+  { Text: "Home", Path: "/" },
+  { Text: "Planering", Path: "/planering" },
+  { Text: "Service Protokol", Path: "/service-protokol" },
+  { Text: "Different services", Path: "/different-services" },
+  { Text: "Cooperate with oss", Path: "/cooperate-with-oss" },
 ];
 
-//------------------------------------------------------------------------------------------//
-
 function App() {
-
   return (
     <>
-      <Navbar GoToNavLink={GoToNavLink} />
-      <Card />
+      <Router>
+          <Navbar GoToNavLink={GoToNavLink} />
+          <Routes>
+            <Route path="/" element={
+            <>
+            <Home />
+            </>
+            } />
+            <Route path="/planering" element={
+              <>
+              <Planering />
+              {/* <DragAndDrop /> */}
+              </>
+            } />
+            <Route path="/service-protokol" element={
+                <>
+                  <Card />
+                  <ServiceProtokol />
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="/different-services" element={<DifferentServices />} />
+            <Route path="/cooperate-with-oss" element={<CooperateWithOss />} />
+          </Routes>
+      </Router>
     </>
   );
 }
